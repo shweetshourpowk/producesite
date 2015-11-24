88fde790c687d494e35c0a56a820094137f515a8
dumbeetle <losotelo@iupui.edu>
2015-11-24 18:07:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,26 +6,22 @@ require_once('php/header.php');
 require_once('php/connect.php');
 ?>
     <!-- all content goes in between these two tags -->
-    <h1>The Vegetable People</h1>
+    
+    <div id="page" class="container">
+        <div id="content">
+            <h1 class="title">The Vegetable People</h1>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim ex eget ullamcorper dictum. Nunc iaculis, nibh eget eleifend tempor, ligula est tincidunt nisi, non laoreet erat est quis justo. Donec in eros metus. Sed consequat et purus eu tempus. Nulla tellus mauris, porta et nunc et, feugiat efficitur arcu. Etiam fermentum, purus vel porttitor dictum, justo lacus lobortis nulla, in vestibulum mi nibh quis nisl. Quisque porta, turpis sed varius semper, magna felis laoreet nunc, vel lobortis odio mi vitae turpis. Integer est nunc, commodo id nulla sit amet, tristique dictum elit. Morbi id aliquet dolor, et scelerisque ex. Etiam pharetra nunc nunc, ut luctus lorem pellentesque id. In pretium odio nec efficitur lobortis. Nullam a mollis diam, nec dictum lorem. Sed semper tincidunt rutrum. Maecenas facilisis commodo elit, non pretium ante. Sed ultrices tellus non velit ultrices, vel bibendum nibh pretium. Vivamus hendrerit imperdiet augue eget vehicula.</p>
 
-<div id="content">
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dignissim ex eget ullamcorper dictum. Nunc iaculis, nibh eget eleifend tempor, ligula est tincidunt nisi, non laoreet erat est quis justo. Donec in eros metus. Sed consequat et purus eu tempus. Nulla tellus mauris, porta et nunc et, feugiat efficitur arcu. Etiam fermentum, purus vel porttitor dictum, justo lacus lobortis nulla, in vestibulum mi nibh quis nisl. Quisque porta, turpis sed varius semper, magna felis laoreet nunc, vel lobortis odio mi vitae turpis. Integer est nunc, commodo id nulla sit amet, tristique dictum elit. Morbi id aliquet dolor, et scelerisque ex. Etiam pharetra nunc nunc, ut luctus lorem pellentesque id. In pretium odio nec efficitur lobortis. Nullam a mollis diam, nec dictum lorem. Sed semper tincidunt rutrum. Maecenas facilisis commodo elit, non pretium ante. Sed ultrices tellus non velit ultrices, vel bibendum nibh pretium. Vivamus hendrerit imperdiet augue eget vehicula.</p>
+            <p>Aenean nec iaculis sem. Aenean malesuada egestas nisi. Nullam vulputate, justo at mattis placerat, massa purus varius sapien, sed bibendum ex diam ac magna. Mauris accumsan pharetra laoreet. Suspendisse ornare pulvinar elementum. Praesent pretium arcu tellus, ut aliquet neque pellentesque non. Duis non orci vitae augue mattis vulputate. Cras ac dolor in ipsum interdum porta a eget mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec laoreet nibh, sed molestie ex. Aliquam vulputate fermentum mi eget volutpat. Morbi eget libero a est vulputate interdum sed eget sem. Praesent porttitor leo vitae rhoncus finibus. Vivamus mattis mollis mauris molestie eleifend. Nam lacinia, felis id feugiat hendrerit, orci dolor pulvinar enim, tempor elementum magna magna condimentum eros.</p>
 
-        <p>Aenean nec iaculis sem. Aenean malesuada egestas nisi. Nullam vulputate, justo at mattis placerat, massa purus varius sapien, sed bibendum ex diam ac magna. Mauris accumsan pharetra laoreet. Suspendisse ornare pulvinar elementum. Praesent pretium arcu tellus, ut aliquet neque pellentesque non. Duis non orci vitae augue mattis vulputate. Cras ac dolor in ipsum interdum porta a eget mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec laoreet nibh, sed molestie ex. Aliquam vulputate fermentum mi eget volutpat. Morbi eget libero a est vulputate interdum sed eget sem. Praesent porttitor leo vitae rhoncus finibus. Vivamus mattis mollis mauris molestie eleifend. Nam lacinia, felis id feugiat hendrerit, orci dolor pulvinar enim, tempor elementum magna magna condimentum eros.</p>
+            <p>Vivamus id ligula a nisl varius vulputate. Proin congue tincidunt scelerisque. Etiam facilisis bibendum massa ut auctor. Integer non efficitur tortor. Aenean facilisis mattis mi, eu pulvinar orci facilisis nec. Integer iaculis est a justo aliquam, faucibus vulputate arcu ultrices. Suspendisse vitae faucibus ligula. Curabitur venenatis hendrerit lacus lacinia lacinia. Curabitur id velit nec nulla vulputate euismod sed vel elit. Mauris tristique tellus urna, at consectetur felis tempor sed. Vivamus ultrices, enim non condimentum ullamcorper, diam ipsum pulvinar massa, quis consequat erat risus eget orci. Phasellus tincidunt turpis in felis laoreet gravida. Praesent gravida massa tempor nulla varius, id fringilla nunc sollicitudin. Morbi aliquet, neque id semper dapibus, ipsum ipsum maximus lectus, nec iaculis elit dolor vitae leo.</p>
 
-        <p>Vivamus id ligula a nisl varius vulputate. Proin congue tincidunt scelerisque. Etiam facilisis bibendum massa ut auctor. Integer non efficitur tortor. Aenean facilisis mattis mi, eu pulvinar orci facilisis nec. Integer iaculis est a justo aliquam, faucibus vulputate arcu ultrices. Suspendisse vitae faucibus ligula. Curabitur venenatis hendrerit lacus lacinia lacinia. Curabitur id velit nec nulla vulputate euismod sed vel elit. Mauris tristique tellus urna, at consectetur felis tempor sed. Vivamus ultrices, enim non condimentum ullamcorper, diam ipsum pulvinar massa, quis consequat erat risus eget orci. Phasellus tincidunt turpis in felis laoreet gravida. Praesent gravida massa tempor nulla varius, id fringilla nunc sollicitudin. Morbi aliquet, neque id semper dapibus, ipsum ipsum maximus lectus, nec iaculis elit dolor vitae leo.</p>
+            <p>Proin elementum sollicitudin nulla eu convallis. Ut tempus vestibulum mi, sed auctor orci. Phasellus pulvinar ornare massa sollicitudin placerat. Phasellus interdum, nibh a porta elementum, nibh lectus elementum justo, a tincidunt neque augue a lorem. In dapibus vel lorem nec aliquam. Vivamus elementum eu erat vel vehicula. Cras non gravida quam.</p>
 
-       <p>Proin elementum sollicitudin nulla eu convallis. Ut tempus vestibulum mi, sed auctor orci. Phasellus pulvinar ornare massa sollicitudin placerat. Phasellus interdum, nibh a porta elementum, nibh lectus elementum justo, a tincidunt neque augue a lorem. In dapibus vel lorem nec aliquam. Vivamus elementum eu erat vel vehicula. Cras non gravida quam.</p>
-
-        <p>Fusce pulvinar libero a ante ullamcorper, nec aliquet est euismod. Aenean tincidunt lectus eu nibh auctor cursus. Pellentesque pellentesque mollis nibh ut tincidunt. Aliquam in maximus nisl. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec ullamcorper ornare velit eget eleifend. Sed placerat lobortis faucibus. Donec quis augue et dui elementum pharetra eget vitae ligula. In hac habitasse platea dictumst. Proin convallis pellentesque lectus, a lacinia nisl porttitor nec. Phasellus id ipsum metus.
-    </p>
-</div>
-
-
-
-
-
+            <p>Fusce pulvinar libero a ante ullamcorper, nec aliquet est euismod. Aenean tincidunt lectus eu nibh auctor cursus. Pellentesque pellentesque mollis nibh ut tincidunt. Aliquam in maximus nisl. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec ullamcorper ornare velit eget eleifend. Sed placerat lobortis faucibus. Donec quis augue et dui elementum pharetra eget vitae ligula. In hac habitasse platea dictumst. Proin convallis pellentesque lectus, a lacinia nisl porttitor nec. Phasellus id ipsum metus.</p>
+        </div>
+    </div>
     <!-- all content goes in between these two tags -->
     <script src="js/script.js"></script>
 <?php
