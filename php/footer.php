8dece6091f0f7b5061c2b33a7a481e8b3fc88658
dumbeetle <losotelo@iupui.edu>
2015-11-24 18:09:11 -0500

    Update footer.php

--- php/footer.php
+++ php/footer.php
@@ -1,11 +1,17 @@
 
-    <footer align="center">
-        <hr />
-        <?php
-        echo date("l, F d, Y", time());
-        ?>
-    </footer>
+<div id="footer-wrapper">
+    <div id="footer" class="container">
+        <div id="box1">
+            <div class="title">
+                <hr />
+                <?php
+                echo date("l, F d, Y", time());
+                ?>
+            </div>
+        </div>
+    </div>
 </div>
+
 
 </body>
 </html>
