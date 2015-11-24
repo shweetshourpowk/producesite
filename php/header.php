90d6e902e591ae96a97f72ccd614ab845b6bdad7
dumbeetle <losotelo@iupui.edu>
2015-11-24 18:09:56 -0500

    Update header.php

--- php/header.php
+++ php/header.php
@@ -8,10 +8,21 @@
 
 <body>
 <div id="wrapper">
-<nav>
-    <a href="index.php">Home</a>
-    <a href="vegetables.php">Vegetables</a>
-    <a href="suppliers.php">Suppliers</a>
-</nav>
-
-
+    <div id="menu-wrapper">
+        <div id="menu" class="container">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="vegetables.php">Vegetables</a></li>
+                <li><a href="suppliers.php">Suppliers</a></li>
+            </ul>
+        </div>
+</div>
+</div>
+<div id="header-wrapper">
+    <div id="header" class="container">
+        <div id="logo">
+            <h1><a href="#">Vegetable Musketeers</a></h1>
+        </div>
+    </div>
+</div>
+<div id="banner"></div>
